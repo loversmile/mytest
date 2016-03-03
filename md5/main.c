5cc857149b92a0e19803dce2751dd4a9190f28b4
jklou <jklou@grandstream.cn>
2016-03-02 19:50:31 -0800

    md5 test

---
+++ md5/main.c
@@ -0,0 +1,36 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 02 Mar 2016 07:30:20 PM PST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "md5.h"
+int main(int argc, char *argv[])
+{
+    MD5_CTX md5;
+    MD5Init(&md5);
+    int i;
+    unsigned char encrypt[] = "userid2016030309555112345";//"admin";//21232f297a57a5a743894a0e4a801fc3
+    unsigned char decrypt[16];
+    MD5Update(&md5,encrypt,strlen((char *)encrypt));
+    MD5Final(&md5,decrypt);
+    printf("加密前:%s\n加密后16位:",encrypt);
+    for(i=4;i<12;i++)
+    {
+        printf("%02x",decrypt[i]);
+    }
+
+    printf("\n加密前:%s\n加密后32位:",encrypt);  
+    for(i=0;i<16;i++)
+    {
+        printf("%02x\n",decrypt[i]);
+    }
+  
+    getchar();
+ 
+    return 0;
+}
