dfb7afcd615e742cf8e57a6ce07d86146fa2e5c8
jklou <jklou@grandstream.cn>
2016-03-02 19:54:48 -0800

    update main.c

--- md5/main.c
+++ md5/main.c
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
     MD5_CTX md5;
     MD5Init(&md5);
     int i;
-    unsigned char encrypt[] = "userid2016030309555112345";//"admin";//21232f297a57a5a743894a0e4a801fc3
+    //unsigned char encrypt[] = "userid2016030309555112345";//"admin";//21232f297a57a5a743894a0e4a801fc3
     unsigned char decrypt[16];
-    MD5Update(&md5,encrypt,strlen((char *)encrypt));
+    MD5Update(&md5,argv[1],strlen((char *)argv[1]));
     MD5Final(&md5,decrypt);
-    printf("加密前:%s\n加密后16位:",encrypt);
+    printf("加密前:%s\n加密后16位:",argv[1]);
     for(i=4;i<12;i++)
     {
         printf("%02x",decrypt[i]);
     }
 
-    printf("\n加密前:%s\n加密后32位:",encrypt);  
+    printf("\n加密前:%s\n加密后32位:",argv[1]);  
     for(i=0;i<16;i++)
     {
-        printf("%02x\n",decrypt[i]);
+        printf("%02x",decrypt[i]);
     }
   
     getchar();
